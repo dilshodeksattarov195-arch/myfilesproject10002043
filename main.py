6f3ac73fd64d3f641c11af9f5e21dbdeaa50c1e3
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 09:32:52 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class emailUalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = emailUalculateWorker()
+    print("Worker engine initialized.")
